725e653ec42679a4197b2aec434cb24856daa916
Dhairya <dhairyamehta129@gmail.com>
2025-12-03 13:36:22 -0500

    Fix:finalllllllllll

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,8 @@
-export function requireLogin(req, res, next) {
-  if (!req.session.user) return res.redirect("/login");
+function requireLogin(req, res, next) {
+  if (!req.session.user) {
+    return res.redirect("/login");
+  }
   next();
 }
+
+module.exports = { requireLogin };
