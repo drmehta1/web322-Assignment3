e3dfd2f75148fe2bd21158840264196b7ff82341
Dhairya <dhairyamehta129@gmail.com>
2025-12-03 13:55:32 -0500

    Fix:finalllllllllllllllllll

--- models/Task.js
+++ models/Task.js
@@ -1,28 +1,21 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/postgres");
 
-let Task;
-
-function getTaskModel() {
-  if (!Task && sequelize) {
-    Task = sequelize.define("Task", {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      description: DataTypes.TEXT,
-      dueDate: DataTypes.DATE,
-      status: {
-        type: DataTypes.STRING,
-        defaultValue: "pending",
-      },
-      userId: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      }
-    });
+const Task = sequelize.define("Task", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: DataTypes.TEXT,
+  dueDate: DataTypes.DATE,
+  status: {
+    type: DataTypes.STRING,
+    defaultValue: "pending"
+  },
+  userId: {
+    type: DataTypes.STRING,
+    allowNull: false
   }
-  return Task;
-}
+});
 
-module.exports = getTaskModel();
+module.exports = { Task };
